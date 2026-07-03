3558a64a236a07900df59b8a54ae48d41adb4356
dev-gh <dev-gh@users.noreply.github.com>
2026-07-03 15:49:29 +0300

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
@@ -0,0 +1,66 @@
+'''
+You are given an array of length n which was originally sorted in ascending order. It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+
+    [3,4,5,6,1,2] if it was rotated 4 times.
+    [1,2,3,4,5,6] if it was rotated 6 times.
+
+Given the rotated sorted array nums and an integer target, return the index of target within nums, or -1 if it is not present.
+
+You may assume all elements in the sorted rotated array nums are unique,
+
+A solution that runs in O(n) time is trivial, can you write an algorithm that runs in O(log n) time?
+
+Example 1:
+
+Input: nums = [3,4,5,6,1,2], target = 1
+
+Output: 4
+
+Example 2:
+
+Input: nums = [3,5,6,0,1,2], target = 4
+
+Output: -1
+
+Constraints:
+
+    1 <= nums.length <= 1000
+    -1000 <= nums[i] <= 1000
+    -1000 <= target <= 1000
+    All values of nums are unique.
+    nums is an ascending array that is possibly rotated.
+
+'''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            # print(f'{nums[left]=}, {nums[right]=}, {nums[mid]=}')
+
+            if nums[left] <= nums[mid]:
+                if target == nums[left]:
+                    return left
+                elif target == nums[mid]:
+                    return mid
+                elif target > nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            elif nums[mid] <= nums[right]:
+                if target == nums[mid]:
+                    return mid
+                elif target == nums[right]:
+                    return right
+                elif target > nums[mid] and target < nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1
+          
+        
