3a77c59f550395fbdde545137db2f9ce6a80eb19
dev-gh <dev-gh@users.noreply.github.com>
2026-09-20 08:37:57 +0300

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.py
@@ -0,0 +1,43 @@
+'''
+You are given an 2-D array points where points[i] = [xi, yi] represents the coordinates of a point on an X-Y axis plane. You are also given an integer k.
+
+Return the k closest points to the origin (0, 0).
+
+The distance between two points is defined as the Euclidean distance (sqrt((x1 - x2)^2 + (y1 - y2)^2)).
+
+You may return the answer in any order. The answer is guaranteed to be unique(except for the order in which the points are returned.)
+
+Example 1:
+
+Input: points = [[0,2],[2,2]], k = 1
+
+Output: [[0,2]]
+
+Explanation : The distance between (0, 2) and the origin (0, 0) is 2. The distance between (2, 2) and the origin is sqrt(2^2 + 2^2) = 2.82842. So the closest point to the origin is (0, 2).
+
+Example 2:
+
+Input: points = [[0,2],[2,0],[2,2]], k = 2
+
+Output: [[0,2],[2,0]]
+
+Explanation: The output [2,0],[0,2] would also be accepted.
+
+Constraints:
+
+    1 <= k <= points.length <= 1000
+    -100 <= points[i][0], points[i][1] <= 100
+
+'''
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points_by_distances = []
+        for x, y in points:
+            #distance_00 = math.sqrt(x ** 2 + y ** 2)
+            distance_00 = x ** 2 + y ** 2
+            heapq.heappush(points_by_distances, (-distance_00, [x, y]))
+            if len(points_by_distances) > k:
+                heapq.heappop(points_by_distances)
+
+        return [point for distance_00, point in points_by_distances]
+        
